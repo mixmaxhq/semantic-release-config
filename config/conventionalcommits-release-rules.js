8d30d32c423ab1cd534225c25114886b64a21209
Logan Davis <logankeahidavis@gmail.com>
2021-05-07 15:34:40 -0700

    feat(config): generate patch releases for nonstandard types
    According to our documentation, we support these types:
    https://github.com/commitizen/conventional-commit-types/blob/master/index.json
    
    However, since we use our release config to extend the defaults
    https://github.com/semantic-release/commit-analyzer/blob/master/lib/default-release-rules.js
    many of these types don't generate associated releases. I've
    been bitten by this a few times, mostly on chore commits.
    
    This commit introduces releases for ci, build, chore, revert,
    and refactor commits to avoid this problem.

--- config/conventionalcommits-release-rules.js
+++ config/conventionalcommits-release-rules.js
@@ -1,6 +1,26 @@
 module.exports = [
   {
+    type: 'build',
+    release: 'patch',
+  },
+  {
+    type: 'chore',
+    release: 'patch',
+  },
+  {
+    type: 'ci',
+    release: 'patch',
+  },
+  {
     type: 'improvement',
     release: 'minor',
+  },
+  {
+    type: 'refactor',
+    release: 'patch',
+  },
+  {
+    type: 'revert',
+    release: 'patch',
   },
 ];
