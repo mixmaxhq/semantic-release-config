a85e6a197ccaf464d7b1e47bcf54e9e8faf0e6e3
Eduard Borges <eduard@mixmax.com>
2023-03-22 14:38:48 -0700

    fix: typo

--- package/index.js
+++ package/index.js
@@ -11,7 +11,7 @@ module.exports = {
         preset: 'conventionalcommits',
         releaseRules: [
           {
-            type: 'build',
+            type: 'chore',
             scope: 'deps',
             release: 'patch',
           },
