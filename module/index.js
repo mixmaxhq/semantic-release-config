155a8eee07bacdce5c4434e6ecefa0d8e2c0fbc6
kristina <kristina@mixmax.com>
2024-03-15 10:52:13 +0100

    feat: add module config

---
+++ module/index.js
@@ -0,0 +1,48 @@
+module.exports = {
+  branches: [
+    '(master|main)',
+    { name: 'beta', prerelease: true },
+    { name: 'alpha', prerelease: true },
+  ],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        releaseRules: [
+          {
+            type: 'chore',
+            scope: 'deps',
+            release: 'patch',
+          },
+        ],
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        presetConfig: {
+          types: [
+            {
+              type: 'feat',
+              section: 'Features',
+            },
+            {
+              type: 'fix',
+              section: 'Bug Fixes',
+            },
+            {
+              type: 'build',
+              section: 'Dependencies and Other Build Updates',
+              hidden: false,
+            },
+          ],
+        },
+      },
+    ],
+    '@semantic-release/changelog',
+    '@semantic-release/github',
+    '@semantic-release/git',
+  ],
+};
